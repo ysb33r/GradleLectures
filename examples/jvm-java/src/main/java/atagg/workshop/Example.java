c292dede834e8bf029ff329e285b6343d92bbb44
Schalk W. Cronjé <ysb33r@gmail.com>
2015-10-06 15:38:15 +0100

    Added first example project

---
+++ examples/jvm-java/src/main/java/atagg/workshop/Example.java
@@ -0,0 +1,11 @@
+class Example {
+    public String setName(final String nm) {
+        name = nm;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+}
