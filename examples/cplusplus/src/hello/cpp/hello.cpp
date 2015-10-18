2e5cf3f46d10a553221b4e7a681f6c550dc3cdc7
Schalk W. Cronjé <ysb33r@gmail.com>
2015-10-18 21:56:08 +0100

    Added C++ project

---
+++ examples/cplusplus/src/hello/cpp/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "hello.hpp"
+
+using namespace std;
+
+int main(int argc,char** argv) {
+  cout << "Hello, this is " << hello::greeting << endl;
+}
+
---
+++ examples/cplusplus/src/hello/headers/hello.hpp
@@ -0,0 +1,8 @@
+#ifndef GRADLE45MIN_HELLO_HPP
+#define GRADLE45MIN_HELLO_HPP
+
+namespace hello {
+    const char* greeting = "Gradle in Polyglot world";
+}
+
+#endif
